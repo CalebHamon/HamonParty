f7d991e8d7c3f18e24cb7dc527c39de72ea06a6f
CalebHamon <123963819+CalebHamon@users.noreply.github.com>
2023-11-20 13:11:31 -0500

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -3,15 +3,18 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class ReadFile {
-  public static void main(String[] args) {
+  public ReadFile() {
     try {
-      File myObj = new File("filename.txt");
+      File myObj = new File("partyguests (1).txt");
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
+			String data = myReader.nextLine();
+			String[] set = new String[4];
+			set = data.split(",");
+			//parse int
+			//roster.add(Attendee)
+		}    
+		myReader.close();
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
