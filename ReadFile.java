616fb4af9b93c2a5ad331ce7e46747048ead658f
CalebHamon <123963819+CalebHamon@users.noreply.github.com>
2023-11-27 11:27:52 -0500

    Added comments

--- ReadFile.java
+++ ReadFile.java
@@ -3,6 +3,15 @@ import java.io.FileNotFoundException; // Import this class to handle errors
 import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 
+/**
+The main method is contained in this class, as well as all of the arraylists
+the main method utilizes all the other methods in the class that are responsible to do all the things that are apart of Party
+including adding a guest, looking them up or looking up a table/company
+These explanations will be more flushed out by each method
+The arraylist comes from constructors from other classes and uses them to keep track of everyone.
+TODO: Should be nothing else left to do in the code
+*/
+
 public class ReadFile {
 
   public static ArrayList<Company> comp = new ArrayList<Company>();
@@ -19,18 +28,18 @@ public class ReadFile {
     String yes = "yes";
     String no = "no";
 
-    addRoster();
+    addRoster(); //Both are reading the files
     addComp();
 
-    for (int i = 0; i < maxTables; i++) {
+    for (int i = 0; i < maxTables; i++) { //creating the ten tables
       table.add(new Table(i + 1));
     }
 
-    for (Attendence person : roster) {
+    for (Attendence person : roster) { //puting everyone from the add roster into the actual arraylist with everyone
       addPrev(person);
     }
 
-    for (int attNum = 0; attNum < maxNum; attNum++) {
+    for (int attNum = 0; attNum < maxNum; attNum++) { //Adding guests y/n
       do {
         System.out.println("Would you like to add a guest? (yes or no)");
         can = scan.nextLine();
@@ -44,7 +53,7 @@ public class ReadFile {
       }
     }
 
-    for (int i = 0; i < maxTables; i++) {
+    for (int i = 0; i < maxTables; i++) {//searching by table y/n
       do {
         System.out.println("Would you like to see a table guest list? (yes or no)");
         can = scan.nextLine();
@@ -56,7 +65,7 @@ public class ReadFile {
       }
     }
 
-    for (int i = 0; i < maxComp; i++) {
+    for (int i = 0; i < maxComp; i++) {//searching by company y/n
       do {
         System.out.println("Would you like to see a company guest list? (yes or no)");
         can = scan.nextLine();
@@ -68,7 +77,7 @@ public class ReadFile {
       }
     }
 
-    for (int i = 0; i < attNum; i++) {
+    for (int i = 0; i < attNum; i++) {//searching for someone y/n
       do {
         System.out.println("Would you like to look someone up? (yes or no)");
         can = scan.nextLine();
@@ -82,6 +91,9 @@ public class ReadFile {
 
   }
 
+  //Read file for the txt of the partyguests
+  //The method scans each line of the txt file, splits them into one line each
+  //the line then is split into an array where it then goes to an array as one object
   public static void addRoster() {
     try {
       File myObj = new File("partyguests.txt");
@@ -100,6 +112,8 @@ public class ReadFile {
     }
   }
 
+
+  //Same thing as the other readfile, except it does the reading for companies rather than people
   public static void addComp() {
     try {
       File myObj = new File("companies.txt");
@@ -118,6 +132,9 @@ public class ReadFile {
     }
   }
 
+
+  //This method checks to see what tables are available for people to sit at
+  //Returning which table number it can sit at or -1 if the table is not found
   public static int checkAva(int compNum) {
     int zero = 0;
     int ten = 10;
@@ -126,13 +143,13 @@ public class ReadFile {
       boolean ava = true; // Reset ava for each table
 
       if (table.get(i).limit() != zero) {
-
-        int rosterSize = table.get(i).whoRoster().size();
+  
+        int rosterSize = table.get(i).whoRoster().size(); //checking each table for ava
         if (rosterSize < ten) {
           for (int g = 0; g < rosterSize; g++) {
             if (table.get(i).whoRoster().get(g).compNum() == compNum) {
               ava = false;
-              break;
+              break; //break if found
             }
           }
           if (ava) {
@@ -147,18 +164,23 @@ public class ReadFile {
     return -1; // No available table found
   }
 
+  //This adds the people to the tables from ava
+  //it passes in a person to then at them to whatever table they need to go to
   public static void addPrev(Attendence person) {
     int placehold;
     placehold = person.compNum();
-    int p2 = checkAva(placehold);
+    int p2 = checkAva(placehold); //returns table number
     if (p2 != -1) {
-      table.get(p2).rostTabAdd(person);
-      table.get(p2).occAdd();
+      table.get(p2).rostTabAdd(person); //adding the person to the table
+      table.get(p2).occAdd(); //adding 1 to the max amount of people at the table
     } else {
-      attNum--;
+      attNum--; //remove the guest if not ava
     }
   }
 
+  //This method adds additional guests to the party
+  //Goes through everything known about the person to add them to a correct table
+  //Uses checkava and addprev to add them to a table
   public static void addGuest() {
     String can;
     Scanner scan = new Scanner(System.in);
@@ -172,14 +194,14 @@ public class ReadFile {
     System.out
         .println("What is the company of the guest you would like to add? (type in the company exactly how it is!)");
     can = scan.nextLine();
-    int compListSize = comp.size();
+    int compListSize = comp.size(); //all prev code is to add to an array what is known about the person
     for (int i = 0; i < compListSize; i++) {
       if (can.equalsIgnoreCase(comp.get(i).compName())) {
         int placeholder = comp.get(i).compNum();
         set[2] = Integer.toString(comp.get(i).compNum());
-        int p2 = checkAva(placeholder);
+        int p2 = checkAva(placeholder); //converting their company to a number
         if (p2 != -1) {
-          Attendence b1 = new Attendence(set[0], set[1], Integer.parseInt(set[2]));
+          Attendence b1 = new Attendence(set[0], set[1], Integer.parseInt(set[2])); //adding the person/object to the arraylist
           roster.add(b1);
           attNum++;
         } else {
@@ -190,6 +212,8 @@ public class ReadFile {
     }
   }
 
+  //The method prints out the table guest list
+  //Using the table arraylist, it is able to print out where everyone is at and where they are seated
   public static void tabGuestList() {
     int bucket;
     do {
@@ -198,12 +222,14 @@ public class ReadFile {
       bucket = scan.nextInt();
     } while (!((bucket > 0) && (bucket <= 10)));
     bucket = bucket - 1;
-    int maxAttTab = table.get(bucket).whoRoster().size();
+    int maxAttTab = table.get(bucket).whoRoster().size(); //arraylist then imbedded in another arraylist to get who is at the table
     for (int g = 0; g < maxAttTab; g++) {
       System.out.println(table.get(bucket).whoRoster().get(g).toString());
     }
   }
 
+  //Same thing as the table guest list only now for companies
+  //Though it is easier as it only needs to convert name to a number and print everyoe by their number
   public static void compGuestList() {
     Scanner scan = new Scanner(System.in);
     String sbucket;
@@ -213,9 +239,9 @@ public class ReadFile {
     System.out.println("Gucci, Prada, Nike, Dodge, Maserati, Razor, AMD");
     sbucket = scan.nextLine();
     for (int g = 0; g < maxComp; g++) {
-      if (sbucket.equalsIgnoreCase(comp.get(g).compName())) {
+      if (sbucket.equalsIgnoreCase(comp.get(g).compName())) { //checking if company real
         for (Attendence person : roster) {
-          if (person.compNum() == comp.get(g).compNum()) {
+          if (person.compNum() == comp.get(g).compNum()) { //finding person associated with company
             System.out.println(person.toString());
           }
         }
@@ -223,20 +249,22 @@ public class ReadFile {
     }
   }
 
+  //Uses last name of person to find where they are seated
+  //Then uses table arraylist to then find where they are by a sequential search
   public static void guestLookUp() {
-    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in); //prompts
     String sbucket;
     System.out.println("Who would you like to look up? (Last Name Only Please)");
     System.out.println("(A typo or person who is not on the list will reprompt the question!)");
     sbucket = scan.nextLine();
-    for (int i = 0; i < maxTables; i++) {
+    for (int i = 0; i < maxTables; i++) { //actual searching
       int tabRostSize = table.get(i).whoRoster().size();
       for (int g = 0; g < tabRostSize ; g++) {
         if(sbucket.equals(table.get(i).whoRoster().get(g).lname())){
           int tabNum = i +1;
           int seatNum = g + 1;
           System.out.println(sbucket + " sits at table " + tabNum + " in seat number " + seatNum);
-          break;
+          break; //break away from list if found
         }
       }
     }
